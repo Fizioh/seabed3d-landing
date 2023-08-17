02757674e284726e627bd785bc2ee58d5cdef41e
Fizioh <batrom@laposte.net>
2023-08-17 20:08:42 +0300

    build: format script

--- backend/scripts/extract_data.py
+++ backend/scripts/extract_data.py
@@ -1,17 +1,14 @@
 from netCDF4 import Dataset
-
-# Ouvrir le fichier NetCDF
-file_path = "chemin/vers/le/fichier.nc"
-dataset = Dataset(file_path, "r")  # Mode lecture ("r")
-
-# Accéder à une variable spécifique
-variable_name = "nom_de_la_variable"
-variable_data = dataset.variables[variable_name][:]  # Obtenir les données sous forme de tableau numpy
-
-# Fermer le fichier après utilisation
-dataset.close()
-
 import json
+
+# Chemin vers le fichier NetCDF
+file_path = "chemin/vers/le/fichier.nc"
+
+# Ouvrir le fichier NetCDF en mode lecture ("r")
+with Dataset(file_path, "r") as dataset:
+    # Accéder à une variable spécifique
+    variable_name = "nom_de_la_variable"
+    variable_data = dataset.variables[variable_name][:]  # Obtenir les données sous forme de tableau numpy
 
 # Traiter les données si nécessaire (exemple : normalisation)
 normalized_data = (variable_data - variable_data.min()) / (variable_data.max() - variable_data.min())
