6602508d04cb28bbf3e8bc434b6c8bfd927fe771
Fizioh <batrom@laposte.net>
2023-08-17 20:07:40 +0300

    feat: cleaning and format of data

--- backend/scripts/extract_data.py
+++ backend/scripts/extract_data.py
@@ -11,3 +11,15 @@ variable_data = dataset.variables[variable_name][:]  # Obtenir les données sous
 # Fermer le fichier après utilisation
 dataset.close()
 
+import json
+
+# Traiter les données si nécessaire (exemple : normalisation)
+normalized_data = (variable_data - variable_data.min()) / (variable_data.max() - variable_data.min())
+
+# Convertir les données en un format JSON
+data_for_threejs = normalized_data.tolist()
+data_json = json.dumps(data_for_threejs)
+
+# Écrire les données dans un fichier JSON
+with open("data_for_threejs.json", "w") as json_file:
+    json_file.write(data_json)
